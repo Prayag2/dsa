3ffa4240b75bf4bad6962eb3c934fd3ebfd5464c
Prayag2 <prayagjain2@gmail.com>
2024-08-11 00:55:41 +0530

    question: 7

---
+++ hash-problems/src/7.cpp
@@ -0,0 +1,22 @@
+// leetcode bank
+// o(n)
+#include <bits/stdc++.h>
+
+int main() {
+  int n {};
+  std::cout << "Enter n: ";
+  std::cin >> n;
+
+  int lastSum {28}, week {static_cast<int>(floor(n/7))};
+  int total {n > 7 ? 28 : 0};
+  for (int i {1}; i < week ; i++) {
+    lastSum += 7; // originally: lastSum = lastSum - i + (i+6)
+    total+=lastSum;
+  }
+
+  for (int i {0}, val {week+1}; i < n-week*7; i++, val++) total += val;
+
+  std::cout << total << std::endl;
+
+  return 0;
+}
